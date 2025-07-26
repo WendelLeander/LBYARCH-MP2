3f7e55237885cfcf4d145e577f1c22fe4c0c4fea
owensanluis <owenphillipsanluis@gmail.com>
2025-07-27 02:54:57 +0800

    added error handling

--- Cfile.c
+++ Cfile.c
@@ -1,37 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-extern void computeAcceleration(float* matrix, int num_cars, int* output);
+void computeAcceleration(float* matrix, int num_cars, int* output);
 
 int main() 
 {
     int Y;
-    printf("Enter number of cars: ");
-    scanf("%d", &Y);
+    char answer[100];
+    char line[256];
 
-    float* matrix = malloc(sizeof(float) * Y * 3);
-    int* output = malloc(sizeof(int) * Y);
+    do{
+        printf("Enter number of cars: ");
+        scanf("%d", &Y);
+        getchar();
 
-    printf("Enter each row per car:\n");
-    printf("Format: InitialVelocity FinalVelocity Time\n");
-    int i;
-    for (i = 0; i < Y; i++) 
-    {
-        printf("Car %d: ", i+1);
-        scanf("%f %f %f", &matrix[i*3], &matrix[i*3+1], &matrix[i*3+2]);
-    }
+        float* matrix = malloc(sizeof(float) * Y * 3);
+        int* output = malloc(sizeof(int) * Y);
 
-    computeAcceleration(matrix, Y, output);
+        printf("Enter each row per car:\n");
+        printf("Format: InitialVelocity FinalVelocity Time\n");
+        for (int i = 0; i < Y; i++) 
+        {
+            int scanned;
+            do 
+            {
+                printf("Car %d: ", i+1);
+                if (!fgets(line, sizeof(line), stdin)) 
+                {
+                    printf("Error reading input.\n");
+                    exit(1);
+                }
+                
+                scanned = sscanf(line, "%f %f %f", &matrix[i*3], &matrix[i*3+1], &matrix[i*3+2]);
+                if (scanned != 3) 
+                {
+                    printf("Invalid input! Please enter exactly three numbers (InitialVelocity FinalVelocity Time).\n");
+                }
+            } while (scanned != 3);
+        }
 
-    printf("Acceleration results:\n");
-    for (i = 0; i < Y; i++) 
-    {
-        printf("%d\n", output[i]);
-    }
+        computeAcceleration(matrix, Y, output);
 
-    free(matrix);
-    free(output);
-    
+        printf("Acceleration results:\n");
+        for (int i = 0; i < Y; i++) 
+        {
+            printf("%d\n", output[i]);
+        }
+
+        free(matrix);
+        free(output);
+
+        while(1)
+        {
+            printf("\nRe-do the program? (yes/no): ");
+            scanf("%s", answer);
+
+            if(strcmp(answer, "yes") == 0 || strcmp(answer, "YES") == 0 || strcmp(answer, "y") == 0 || strcmp(answer, "Y") == 0 ||
+                strcmp(answer, "no") == 0 || strcmp(answer, "NO") == 0 || strcmp(answer, "n") == 0 || strcmp(answer, "N") == 0)
+                break;
+            
+            printf("Please answer only 'yes' or 'no'.");
+        }
+        
+    } while(strcmp(answer, "yes") == 0 || strcmp(answer, "YES") == 0 || strcmp(answer, "y") == 0 || strcmp(answer, "Y") == 0);
+
     return 0;
 }
--- sampleC.c
+++ sampleC.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 void computeAcceleration(float* matrix, int num_cars, int* output);
@@ -7,30 +8,47 @@ void computeAcceleration(float* matrix, int num_cars, int* output);
 int main() 
 {
     int Y;
-    printf("Enter number of cars: ");
-    scanf("%d", &Y);
+    char answer[100];
 
-    float* matrix = malloc(sizeof(float) * Y * 3);
-    int* output = malloc(sizeof(int) * Y);
+    do{
+        printf("Enter number of cars: ");
+        scanf("%d", &Y);
 
-    printf("Enter each row per car:\n");
-    printf("Format: InitialVelocity FinalVelocity Time\n");
-    for (int i = 0; i < Y; i++) 
-    {
-        printf("Car %d: ", i+1);
-        scanf("%f %f %f", &matrix[i*3], &matrix[i*3+1], &matrix[i*3+2]);
-    }
+        float* matrix = malloc(sizeof(float) * Y * 3);
+        int* output = malloc(sizeof(int) * Y);
 
-    computeAcceleration(matrix, Y, output);
+        printf("Enter each row per car:\n");
+        printf("Format: InitialVelocity FinalVelocity Time\n");
+        for (int i = 0; i < Y; i++) 
+        {
+            printf("Car %d: ", i+1);
+            scanf("%f %f %f", &matrix[i*3], &matrix[i*3+1], &matrix[i*3+2]);
+        }
 
-    printf("Acceleration results:\n");
-    for (int i = 0; i < Y; i++) 
-    {
-        printf("%d\n", output[i]);
-    }
+        computeAcceleration(matrix, Y, output);
 
-    free(matrix);
-    free(output);
-    
+        printf("Acceleration results:\n");
+        for (int i = 0; i < Y; i++) 
+        {
+            printf("%d\n", output[i]);
+        }
+
+        free(matrix);
+        free(output);
+
+        while(1)
+        {
+            printf("Re-do the program? (yes/no)");
+            scanf("%s", answer);
+
+            if(strcmp(answer, "yes") != 0 || strcmp(answer, "YES") != 0 || strcmp(answer, "y") != 0 || strcmp(answer, "Y") != 0 ||
+                strcmp(answer, "no") == 0 || strcmp(answer, "NO") == 0 || strcmp(answer, "n") == 0 || strcmp(answer, "N") == 0)
+                break;
+            
+            printf("Please answer only 'yes' or 'no'.\n");
+        }
+        
+    } while(strcmp(answer, "yes") == 0 || strcmp(answer, "YES") == 0 || strcmp(answer, "y") == 0 || strcmp(answer, "Y") == 0);
+
     return 0;
 }
